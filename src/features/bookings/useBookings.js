e437b6d495aa0369483c21e1a4fee372cceaeba7
Ebrahim <naser.najafi1988@gmail.com>
2023-12-28 15:25:26 +0100

    adding pre-fetching to booking pages

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { getBookings } from "../../services/apiBookings";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  // FOR Pre=Fetching we need useQueryClient
+  const queryClient = useQueryClient();
+
+  // To get params form URL
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -28,6 +33,7 @@ export function useBookings() {
 
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const { isLoading, data, error } = useQuery({
     queryKey: ["bookings", filter, sortBy, page], // by adding filter to the list when we select different filter, the data will refetch from server
     queryFn: () => getBookings({ filter, sortBy, page }),
@@ -36,6 +42,23 @@ export function useBookings() {
   // Perform null or undefined check for data
   const bookings = data?.data; // Assuming data is an object with a data property
   const count = data?.count;
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  // PRE_FETCHING
+  // it is work like useQuery
+  // PRE-fetch for next page
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  // Pre-fetch for previous page
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["booking", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { bookings, isLoading, error, count };
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // number of items that can be display for pagination on each page
-export const PAGE_SIZE = 10;
+export const PAGE_SIZE = 4;
